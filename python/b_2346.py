b85aa3d32ddb4a0cab97ad2ad93f8c5c74704a7e
김구태 <gootae0220@naver.com>
2024-10-03 20:11:51 +0900

    10/3 end

--- python/b_2346.py
+++ python/b_2346.py
@@ -4,7 +4,25 @@ sys.stdin = open('input.txt')
 n = int(input())
 arr = list(map(int,input().split()))
 
+lst = []
 
+for i in range(len(arr)):
 
+    lst.append([i+1, arr[i]])
+result = []
 
+idx = 0
 
+while lst:
+    num, move = lst.pop(idx)
+    result.append(num)
+
+    if not lst:
+        break
+
+    if move > 0:
+        idx = (idx + (move - 1)) % len(lst)
+    else:
+        idx = (idx + move) % len(lst)
+
+print(*result)
