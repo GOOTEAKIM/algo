71801239339ba1090a0a53a4ef63cea451dac214
김구태 <gootae0220@naver.com>
2024-12-05 22:33:11 +0900

    add programmers codingtest intro

---
+++ C++/programmers_codingtest_intro/programmers_codingtest_intro/p_120804.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+	return 0;
+
+}
+
+int solution(int num1, int num2) {
+	return num1 * num2;
+}
+
