329b377524d44ded18aff00fc61bd402f89444d4
김구태 <gootae0220@naver.com>
2025-01-25 12:27:19 +0900

    [C++] end : softeer [1/25]

---
+++ C++/softeer/softeer/s_100459.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> arr) {
+
+	int N = arr.size();
+
+	vector<int> dp(N, 1);
+
+	for (int i = 1; i < N; i++) {
+		for (int j = 0; j < i; j++) {
+			if (arr[i] > arr[j]) {
+				dp[i] = max(dp[i], dp[j] + 1);
+			}
+		}
+	}
+
+	return *max_element(dp.begin(), dp.end());
+}
+
+int solution(vector<int> arr);
+
+int main() {
+
+	int N;
+
+	cin >> N;
+	
+	vector<int> ans(N);
+
+	for (int i = 0; i < N; i++) {
+		cin >> ans[i];
+	}
+
+	int result = solution(ans);
+
+	cout << result << endl;
+
+	return 0;
+}
+
+1 1 1 1 1
+2
