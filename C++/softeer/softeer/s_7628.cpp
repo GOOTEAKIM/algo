1514fac085bc85bd250545114c3f92b4629fb09e
김구태 <gootae0220@naver.com>
2025-01-27 19:58:33 +0900

    [C++] end : softeer [1/27]

---
+++ C++/softeer/softeer/s_7628.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+	int n;
+
+	cin >> n;
+
+	vector<int> ans;
+
+	for (int i = 0; i < n; i++) {
+		int num;
+
+		cin >> num;
+
+		ans.push_back(num);
+
+	}
+
+	return 0;
+}
