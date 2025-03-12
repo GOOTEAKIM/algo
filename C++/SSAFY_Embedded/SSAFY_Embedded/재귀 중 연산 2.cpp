f58f858206a2fb84abcc3251c2085b9c34199b70
김구태 <gootae0220@naver.com>
2025-03-12 15:40:54 +0900

    [C++] fix : SSAFY Embedded 재귀 중 연산 2

--- C++/SSAFY_Embedded/SSAFY_Embedded/재귀 중 연산 2.cpp
+++ C++/SSAFY_Embedded/SSAFY_Embedded/재귀 중 연산 2.cpp
@@ -1,32 +1,108 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
-int N;
+int N, K;
 
-void func(int now) {
+int arr[1000];
 
-	// 2. 종료조건
-	if (N == now) {
+int path[10]; // 경로
+
+/*
+void func(int now, int sum) {
+
+	// 2. 기저 조건 // base case // 언제 멈출 것인가?
+	if (now >= K) { // k-1 까지 뽑고 k 부터는 뽑지 마라
+
+		for (int i = 0; i < K; i++) {
+
+			cout << path[i] << " ";
+		}
+		cout << " = " << sum << endl;
 		return;
 	}
 
-	// 3-1. 다음으로 진행하면서 실행되는 코드
-	cout << now << " ";
+	// 1. 재귀 기본
+	for (int i = 0; i < N; i++) {
 
-	// 1. 기본 확인 재귀 호출
-	func(now + 1);
+		// 3. 앞으로 진행하면서 실행되는 코드
+		path[now] = arr[i];	// index = now 번째, value = 뽑은 수
+		 
+		func(now + 1, sum + arr[i]); // 가라~
+	}
 
-	// 3-2. 다시 돌아오면서 실행되는 코드
-	cout << now << " ";
+}
+*/
+
+int sum = 0;
+
+void func(int now) {
+
+	// 2. 기저 조건, 언제 멈출 것인가?
+	if (now >= K) {
+
+		int flag = 0; // 잘못된 형태인가?
+
+		for (int i = 0; i < K-1; i++) { 
+
+			if (abs(path[i] - path[i+1]) == 1) { // 인접하는 수의 차이가 1이다?
+				flag = 1; // 잘못된 형태이다.
+			}
+
+		}
+
+		if (!flag) {
+			for (int i = 0; i < K; i++) {
+				cout << path[i] << " ";
+			}
+			cout << " = " << sum << "\n";
+		}
+		return;
+	}
+
+	// 1. 재귀 기본
+
+	for (int i = 0; i < N; i++) {
+
+		// 5. 판별, 이 선택을 하는게 맞는가?
+
+		// 3. 계산 및 처리
+		path[now] = arr[i];
+		
+		sum += arr[i];
+
+		func(now + 1); // 가라
+
+		// 4. 복구
+
+		sum -= arr[i]; // 계산을 복구
+
+		path[now] = 0; // 0으로 초기화
+	}
 
 }
 
+
 int main() {
 
-	N = 5;
+	cin >> N >> K; // n 개 정수 중 k 개 뽑는다
+	 
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+	//func(0, 0);
+
 	func(0);
 
 	return 0;
 }
+
+/*
+input
+
+4 3
+1 2 3 4
+
+*/
