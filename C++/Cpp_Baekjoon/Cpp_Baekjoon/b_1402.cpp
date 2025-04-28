c9b72169decae85fcf7a55d3579738123f2d7654
김구태 <gootae0220@naver.com>
2025-04-28 15:29:13 +0900

    [C++] end : Baekjoon [4/28]

---
+++ C++/Cpp_Baekjoon/Cpp_Baekjoon/b_1402.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+	// 모든 케이스가 yes임
+
+	int t;
+
+	cin >> t;
+
+	while (t--) {
+
+		long long  a, b;
+
+		cin >> a >> b;
+
+		cout << "yes" << "\n";
+	}
+
+	// 1,-1, 1,1 이런 식으로 합을 어떤 수든 다 만들 수 있음
+}
