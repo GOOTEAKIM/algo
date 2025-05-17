3a810a54defda39baca30922e06b31f5cc0b9839
김구태 <gootae0220@naver.com>
2025-05-17 23:36:30 +0900

    [C++] end : programmers lv2 [5/17]

--- C++/programmers_codingtest_lv2/programmers_codingtest_lv2/p_43165.cpp
+++ C++/programmers_codingtest_lv2/programmers_codingtest_lv2/p_43165.cpp
@@ -23,10 +23,37 @@ void dfs(vector<int> numbers, int target, int sum, int idx) {
 
 }
 
+void bfs(vector<int> numbers, int target) {
+
+    queue<pair<int, int>> q;
+
+    q.push({ 0, 0 });
+
+    while (!q.empty()) {
+
+        int sum = q.front().first;
+        int idx = q.front().second;
+        q.pop();
+
+        if (idx == numbers.size()) {
+            if (sum == target) {
+                ans++;
+            }
+        }
+        else {
+            // 다음 숫자를 더하거나 빼서 큐에 추가
+            q.push({ sum + numbers[idx], idx + 1 });
+            q.push({ sum - numbers[idx], idx + 1 });
+        }
+    }
+
+}
 
 int solution(vector<int> numbers, int target) {
 
 	dfs(numbers, target, 0, 0);
+
+    //bfs(numbers, target);
 
 	return ans;
 
