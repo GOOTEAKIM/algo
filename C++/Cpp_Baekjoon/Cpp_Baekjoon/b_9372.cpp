08fca066f8c31050b7c53d4a0ed77d00020251dc
김구태 <gootae0220@naver.com>
2025-05-30 21:02:22 +0900

    [C++] end : Baekjoon [5/30]

---
+++ C++/Cpp_Baekjoon/Cpp_Baekjoon/b_9372.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+
+int main() {
+
+	int t;
+
+	cin >> t;
+
+	while (t--) {
+
+		cin >> n >> m;
+
+		for (int i = 0; i < m; i++) {
+			int a, b;
+
+			cin >> a >> b;
+		}
+
+		cout << n - 1 << "\n";
+	}
+
+	return 0;
+}
+
