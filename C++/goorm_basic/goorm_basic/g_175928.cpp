ee01611aaa3e4abf8243fa8bb113b91fe593f9d8
김구태 <gootae0220@naver.com>
2025-07-15 21:24:25 +0900

    [C++] fix : goorm_baisc g_175928 [7/15]

--- C++/goorm_basic/goorm_basic/g_175928.cpp
+++ C++/goorm_basic/goorm_basic/g_175928.cpp
@@ -5,10 +5,10 @@
 
 using namespace std;
 
-vector<int> ans(4);   // answer -> ans
-vector<int> guess(4); // expectation -> guess
+vector<int> ans(4); // 정답
+vector<int> guess(4); // 예상 값
 
-bool isMatch() {
+bool isMatch() { // 정답과 같은가?
 
     for (int i = 0; i < 4; ++i) {
         if (ans[i] != guess[i]) return false;
@@ -22,11 +22,11 @@ int main() {
     cin >> answer >> order;
 
     for (int i = 0; i < 4; ++i) {
-        ans[i] = answer[i] - '0';
+        ans[i] = answer[i] - '0'; 
         guess[i] = order[i] - '0';
     }
 
-    if (isMatch()) {
+    if (isMatch()) { // 처음에 바로 정답과 같다면?
         cout << 1 << endl;
     }
 
@@ -34,50 +34,60 @@ int main() {
 
         int cnt = 1;
 
-        while (!isMatch()) {
+        while (!isMatch()) { // 만족할 때까지 반복
 
-            vector<int> strike_idx; 
-            bool hasBall = false; 
+            vector<int> strike_idx; // 스트라이크의 위치
+
+            bool isBall = false; // 볼인가?
 
             for (int i = 0; i < 4; ++i) {
                 if (guess[i] == ans[i]) {
-                    strike_idx.push_back(i);
+                    strike_idx.push_back(i); // 스트라이크 위치 파악
                     continue;
                 }
 
-                if (find(ans.begin(), ans.end(), guess[i]) == ans.end()) {
+                if (find(ans.begin(), ans.end(), guess[i]) == ans.end()) { // fail?
+
                     int newNum = guess[i];
-                    while (find(guess.begin(), guess.end(), newNum) != guess.end()) {
+                    
+                    while (find(guess.begin(), guess.end(), newNum) != guess.end()) { // fail 규칙에 따라 다른 위치에 존재하지 않을 때까지 +1
                         newNum = (newNum + 1) % 10;
                     }
-                    guess[i] = newNum;
+
+                    guess[i] = newNum; // 위치 고정
                     continue;
                 }
-                hasBall = true;
+                isBall = true; // 볼이다!
             }
 
-            if (hasBall) {
-                vector<int> stNums;
+            if (isBall) { // 볼이라면
+                vector<int> stNums; // 스트라이크 숫자 모음
                 for (int idx : strike_idx) {
                     stNums.push_back(guess[idx]);
                 }
 
                 for (int s : stNums) {
-                    auto it = find(guess.begin(), guess.end(), s);
-                    if (it != guess.end()) {
-                        guess.erase(it);
+                    auto it = find(guess.begin(), guess.end(), s); // 스트라이크 숫자가 있는 guess의 인덱스 파악
+
+                    if (it != guess.end()) { // 오른쪽 맨 끝에 있는게 아니라면?
+
+                        guess.erase(it); // 삭제
+
                     }
                 }
 
+                // 맨 끝이라면?
                 int last = guess.back();
+
                 guess.pop_back();
+
                 guess.insert(guess.begin(), last);
 
                 for (size_t i = 0; i < strike_idx.size(); ++i) {
-                    guess.insert(guess.begin() + strike_idx[i], stNums[i]);
+                    guess.insert(guess.begin() + strike_idx[i], stNums[i]); // 스트라이크를 원래 자리에 넣는다
                 }
             }
-            cnt++;
+            cnt++; // 카운팅
         }
 
         cout << cnt << endl;
