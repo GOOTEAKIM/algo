d3db05337538454d38a8353f3c71a88d9624bdd5
김구태 <gootae0220@naver.com>
2025-10-30 21:00:00 +0900

    [C++] end : goorm [10/30]

---
+++ C++/goorm/goorm/g_159177.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int n;
+
+	cin >> n;
+
+	long long ans = 1;
+
+	for (int i = 0; i < n; i++) {
+		long long num;
+
+		cin >> num;
+
+		ans *= num;
+	}
+
+	cout << ans;
+
+	return 0;
+}
