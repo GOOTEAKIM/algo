e2f8afcb18d602efa51feb4646dae84263f28239
김구태 <gootae0220@naver.com>
2026-02-09 20:00:00 +0900

    [C++] end : goorm [2/9]

---
+++ C++/goorm/goorm/g_350618.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+	string num;
+
+	cin >> num;
+
+	sort(num.rbegin(), num.rend());
+
+	cout << num;
+
+	return 0;
+}
