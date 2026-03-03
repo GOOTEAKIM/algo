81e6e055c426e92557cec8fb72d9044196fc5143
김구태 <gootae0220@naver.com>
2026-03-03 19:27:09 +0900

    [C++] end : Baekjoon [3/3]

---
+++ C++/Cpp_Baekjoon/Cpp_Baekjoon/b_15720.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+int main() {
+
+	int b, c, d;
+
+	cin >> b >> c >> d;
+
+	vector<int> burger(b),side(c),soda(d);
+
+	for (int i = 0; i < b; i++) cin >> burger[i];
+	for (int i = 0; i < c; i++) cin >> side[i];
+	for (int i = 0; i < d; i++) cin >> soda[i];
+
+	sort(burger.rbegin(), burger.rend());
+	sort(side.rbegin(), side.rend());
+	sort(soda.rbegin(), soda.rend());
+
+	int minimum = min({b,c,d});
+
+	int for_sale = 0;
+
+	for (int i = 0; i < minimum; i++) for_sale += (burger[i] + side[i] + soda[i]);
+
+	int not_sale = for_sale;
+
+	for_sale *= 0.9;
+
+	for (int i = minimum; i < b; i++) not_sale += burger[i], for_sale += burger[i];
+	for (int i = minimum; i < c; i++) not_sale += side[i], for_sale += side[i];
+	for (int i = minimum; i < d; i++) not_sale += soda[i], for_sale += soda[i];
+
+	cout << not_sale << "\n" << for_sale;
+
+	return 0;
+}
