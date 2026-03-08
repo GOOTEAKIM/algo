461ee19def8c47d18327162048bdbbd6570d5cbc
김구태 <gootae0220@naver.com>
2026-03-08 20:00:00 +0900

    [C++] end : goorm [3/8]

---
+++ C++/goorm/goorm/g_351360.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+	int n;
+
+	cin >> n;
+
+	vector<int> a(n);
+
+	vector<int> b(n);
+
+	for (int i = 0; i < n; i++) cin >> a[i];
+
+	for (int i = 0; i < n; i++) cin >> b[i];
+
+	long long maximum = 0;
+
+	for (int i = 0; i < n; i++) {
+
+		if (1LL * a[i] * b[i] > maximum) {
+			maximum = a[i] * b[i];
+		}
+
+	}
+
+	cout << maximum;
+
+	return 0;
+}
