728d9dd606595185022fab22b5253dd3c43112cf
김구태 <gootae0220@naver.com>
2026-03-13 20:00:00 +0900

    [C++] fix : goorm [3/13]

--- C++/goorm/goorm/g_48128.cpp
+++ C++/goorm/goorm/g_48128.cpp
@@ -13,25 +13,24 @@ int main() {
 	int A = d;
 
 	if ((C %= c) == 0 || (B %= b) == 0 || (A %= a) == 0) {
+
 		cout << 1;
 		return 0;
+
 	}
 
 	while (d >= a) {
 
-		if (d >= c) {
-			d %= c;
-		}
-		else if (d >= b) {
-			d %= b;
-		}
-		else {
-			d %= a;
-		}
-
+		if (d >= c) d %= c;
+		
+		else if (d >= b) d %= b;
+		
+		else d %= a;
+		
 	}
 
 	if (d == 0) cout << 1;
+
 	else cout << 0;
 
 	return 0;
