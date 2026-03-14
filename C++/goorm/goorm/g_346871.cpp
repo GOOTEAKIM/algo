ac51b03f156ead31eff206eededd389c93cca87c
김구태 <gootae0220@naver.com>
2026-03-14 20:00:00 +0900

    [C++] fix : goorm [3/14]

--- C++/goorm/goorm/g_346871.cpp
+++ C++/goorm/goorm/g_346871.cpp
@@ -12,21 +12,11 @@ int main() {
 
 	int len = word.length();
 
-	if (n == 1) {
+	if (n == 1) cout << word[len - 2];
 
-		cout << word[len - 2];
+	else if (n == len) cout << word[1];
 
-	}
-	else if (n == len) {
-
-		cout << word[1];
-
-	}
-	else {
-
-		cout << word[len - n - 1] << word[len - n + 1];
-
-	}
+	else cout << word[len - n - 1] << word[len - n + 1];
 
 	return 0;
 }
