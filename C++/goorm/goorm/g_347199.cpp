b21041dc1aab48bfba4ffaf485a3519228d2ee40
김구태 <gootae0220@naver.com>
2026-04-06 17:25:13 +0900

    [C++] end : goorm [4/6]

---
+++ C++/goorm/goorm/g_347199.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int n;
+
+	cin >> n;
+
+	int ans = 0;
+
+	for (int i = 0; i < n; i++) {
+
+		int num;
+
+		cin >> num;
+
+		if (num % 2 == 0) ans++;
+
+	}
+
+	cout << ans;
+
+	return 0;
+}
