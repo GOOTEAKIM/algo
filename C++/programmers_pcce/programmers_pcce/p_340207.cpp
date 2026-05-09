1abfe289b5160a79c23e00b88df359443c5479b8
김구태 <gootae0220@naver.com>
2026-05-09 23:29:02 +0900

    [C++] end : programmers_pcce [5/9]

---
+++ C++/programmers_pcce/programmers_pcce/p_340207.cpp
@@ -0,0 +1,32 @@
+/*
+
+빈칸 채우기 문제 안내
+빈칸 채우기는 이미 완성된 코드 중 빈칸에 알맞은 코드를 입력하는 문제 타입입니다.
+빈칸을 제외한 기본 코드는 수정할 수 없습니다.
+빈칸을 채우지 않을 경우, 실행 결과에 에러 메시지가 표시됩니다.
+
+*/
+
+/*
+
+3
+2
+1
+Let's go!
+
+*/
+
+#include <iostream>
+
+using namespace std;
+int main(void) {
+
+    //string message = " 빈칸 ";
+    string message = "Let's go!";
+
+        //cout << "3 빈칸 2 빈칸 1" << endl;
+        cout << "3\n2\n1" << endl;
+        cout << message << endl;
+
+    return 0;
+}
